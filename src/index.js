4e9379a37422e5e38d3af4db77e638b508a9bfe1
moses <91048963+bemoses@users.noreply.github.com>
2022-08-01 14:22:04 +0000

    first

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+// library/ package imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Amplify } from 'aws-amplify';
+
+// stylesheet imports
+import './styles/index.css';
+
+// file imports
+import AWS_AMPLIFY_CONFIG from './aws-exports';
 import App from './App';
+
+// initialisations
+Amplify.configure(AWS_AMPLIFY_CONFIG);
 
 ReactDOM.render(
   <React.StrictMode>
