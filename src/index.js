a0c575332e2a542bf4aa4dd272b0e45d881588b2
mo-essien <essien.m.q@gmail.com>
2022-08-07 00:39:01 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 // library/ package imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Amplify } from 'aws-amplify';
 
 // stylesheet imports
 import './styles/index.css';
 
 // file imports
-import AWS_AMPLIFY_CONFIG from './aws-exports';
 import App from './App';
-
-// initialisations
-Amplify.configure(AWS_AMPLIFY_CONFIG);
 
 ReactDOM.render(
   <React.StrictMode>
